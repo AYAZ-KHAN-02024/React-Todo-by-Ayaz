756effcc132a96cc8eef3586e6d0b37b560cd7cc
AYAZ-KHAN-02024 <aladdin0999099@gmail.com>
2024-02-13 12:18:32 +0530

    add vite config line

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ToDo_by_ayaz",
+  base: "https://github.com/AYAZ-KHAN-02024/React-Todo-by-Ayaz",
   plugins: [react()],
 })
